09d17992550393c342003adb612a286dd20a1722
NicollyEising <105758162+NicollyEising@users.noreply.github.com>
2025-11-30 07:09:24 -0300

    Add files via upload

--- meu-app/src/item.jsx
+++ meu-app/src/item.jsx
@@ -15,8 +15,8 @@ function Item() {
   const [sidebarHidden, setSidebarHidden] = useState(false);
   
 
-  const backendURL = "http://18.216.31.10:8000";
-  const imagensURL = "http://18.216.31.10:5000";
+  const backendURL = "https://18.216.31.10:8000";
+  const imagensURL = "https://18.216.31.10:5000";
 
   const toggleSidebar = () => setSidebarAberto(!sidebarAberto);
 
